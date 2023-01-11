eb240c4a3ee74848949e55929975dc21c734b46f
Oladipo Bolaji John <bolaji-oladipo@uiowa.edu>
2023-01-10 23:35:56 -0600

    Add files via upload

---
+++ Codes/PQ Day 10.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 10 23:16:57 2023
+
+@author: boladipo
+"""
+
+list1 = [1, 2, 3, 4, 5]
+
+print(5.0 in list1, end=" ")
+print('5' in list1)
