e8174a2a413e7c29f3280ac2590277e15da458cc
Oladipo Bolaji John <bolaji-oladipo@uiowa.edu>
2023-01-10 23:35:31 -0600

    Add files via upload

---
+++ Codes/PQ Day 9.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 10 23:16:17 2023
+
+@author: boladipo
+"""
+
+
+
+
+s1 = {2, 3, 1, 6, 9}
+
+s2 = {1, 3, 5, 7}
+
+a = s1 ^ s2
+
+print(a)
+
+
+
+s3 = {1, 1, 2, 2, 3}
+
+s4 = {1, 1, 2, 2, 4}
+
+b = s3^ s4
+
+print(b)
