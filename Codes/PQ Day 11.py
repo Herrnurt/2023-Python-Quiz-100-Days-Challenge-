9135058ef778edb271ff970d1af919dfdd302588
Oladipo Bolaji John <bolaji-oladipo@uiowa.edu>
2023-01-11 23:55:41 -0600

    Add files via upload

---
+++ Codes/PQ Day 11.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 11 23:45:01 2023
+
+@author: boladipo
+"""
+
+## Set
+
+
+set1 = {1, 2, 3, 4, 5}
+set2 = {3,4,5,6,7}
+
+
+print(set1.union(set2))
+
+
+
+print(set1.intersection(set2))
