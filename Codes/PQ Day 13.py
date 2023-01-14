76a278137f0c39052e1e57b5a789ba3e77fdb618
Oladipo Bolaji John <bolaji-oladipo@uiowa.edu>
2023-01-13 22:01:39 -0600

    Add files via upload

---
+++ Codes/PQ Day 13.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 13 21:53:24 2023
+
+@author: boladipo
+"""
+
+def sign(num):
+    if(num > 0):
+        print('Positive')
+    elif(num < 0):
+        print('Negative')
+    else:
+        print("Zero")
+
+
+sign(0)
