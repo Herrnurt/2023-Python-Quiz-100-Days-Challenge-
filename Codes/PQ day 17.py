ad7f048aa5cc191abfe17085152e18d44de90f7f
Oladipo Bolaji John <bolaji-oladipo@uiowa.edu>
2023-01-17 22:51:35 -0600

    Add files via upload

---
+++ Codes/PQ day 17.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 17 22:31:43 2023
+
+@author: boladipo
+"""
+
+list1 = [5, 't', 5.6, 3, 'PythonGeeks']
+
+print(3.0 in list1, end=" ")
+print(bool(list[-2:-4]))
