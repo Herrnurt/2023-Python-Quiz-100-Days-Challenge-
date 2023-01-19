94ecd70ee82f62c29e49c7226b4c5d243889ddca
Oladipo Bolaji John <bolaji-oladipo@uiowa.edu>
2023-01-18 21:23:14 -0600

    Add files via upload

---
+++ Codes/PQ day 18.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 18 21:18:25 2023
+
+@author: boladipo
+"""
+
+s = 'programming'
+print(s.partition('r'))
+print(s.partition('a'))
+print(s.partition('m'))
