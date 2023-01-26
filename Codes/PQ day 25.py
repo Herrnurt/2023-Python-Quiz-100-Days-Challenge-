5f9fbbfd464ad0f0fd9cb34bea9b3e147a9a5c79
Oladipo Bolaji John <bolaji-oladipo@uiowa.edu>
2023-01-25 19:09:16 -0600

    Add files via upload

---
+++ Codes/PQ day 25.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 25 18:56:48 2023
+
+@author: boladipo
+"""
+
+s = [3,2, 6, 7, 2, 5, 3, 1, -1, 4]
+n = [val for val in s if val%2 != 0]
+
+print(len(n))
