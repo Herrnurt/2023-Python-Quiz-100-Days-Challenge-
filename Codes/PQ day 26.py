f9f466050233dc591169770ac905a67950f2e1b3
Oladipo Bolaji John <bolaji-oladipo@uiowa.edu>
2023-01-26 23:21:04 -0600

    Add files via upload

---
+++ Codes/PQ day 26.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 26 23:11:57 2023
+
+@author: boladipo
+"""
+
+s1 = {'c', 'b', 'd'}
+s2 = {'c', 'b', 'd'}
+
+
+
+print(s2 <= s1)
