8a7ccb172ec787d8f0fea47071f8136460a92581
Oladipo Bolaji John <bolaji-oladipo@uiowa.edu>
2023-01-28 22:28:01 -0600

    Add files via upload

---
+++ Codes/PQ day 28.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 28 21:54:04 2023
+
+@author: boladipo
+"""
+
+
+
+for i in zip((1, 3, 5, 7, 9)):
+    print(i, end=(" "))
+    #print(i)
+    
+    
+    
+    
+    
+    
+a = [1, 3, 5, 7, 9]
+b = ['a', 'b', 'c', 'd', 'e']
+
+for j in zip(a, b):
+    print(j, end = [' '])
