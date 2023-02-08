e4e42f96e0ab4aebed63d1561d930aae18f2f0c4
Oladipo Bolaji John <bolaji-oladipo@uiowa.edu>
2023-02-07 22:24:43 -0600

    Add files via upload

---
+++ Codes/PQ day 38.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  7 22:04:12 2023
+
+@author: boladipo
+"""
+
+
+
+for l, v in enumerate(['s', 'h', 'i', 'n', 'e']):
+    print(l, v, sep = '-', end=';')
+    
+
+
+
+
+for i, j in enumerate(['s', 'h', 'i', 'n', 'e']):
+    print(i, j)
