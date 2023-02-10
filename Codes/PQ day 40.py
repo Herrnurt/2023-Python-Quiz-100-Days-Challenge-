279b72a812b2d52e9e0a963e3f85f2536dee59a2
Oladipo Bolaji John <bolaji-oladipo@uiowa.edu>
2023-02-09 23:07:24 -0600

    Add files via upload

---
+++ Codes/PQ day 40.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  9 22:41:44 2023
+
+@author: boladipo
+"""
+
+months = ['Feb', 'Mar']
+print(list(enumerate(months, start = 2)))
