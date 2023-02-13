1576bb9ee45ebb522d0f9446cda1fd94ae7fffb1
Oladipo Bolaji John <bolaji-oladipo@uiowa.edu>
2023-02-12 22:45:28 -0600

    Add files via upload

---
+++ Codes/PQ Day 43.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 12 22:29:17 2023
+
+@author: boladipo
+"""
+
+a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+print(a[-3: ])
+print(a[-5: -2])
