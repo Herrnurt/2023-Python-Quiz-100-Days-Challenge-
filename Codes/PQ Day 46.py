d0c6b8c03e45a823d28f5994c8d12f84446ebdb2
Oladipo Bolaji John <bolaji-oladipo@uiowa.edu>
2023-02-15 23:57:41 -0600

    Add files via upload

---
+++ Codes/PQ Day 46.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 15 23:43:48 2023
+
+@author: boladipo
+"""
+
+num = [1, 'Python', -5, [], 9]
+
+print(any(num), all(num))
+
+
+word = ['Boy', 'girl', 'woman', 1, []]
+print(all(word))
