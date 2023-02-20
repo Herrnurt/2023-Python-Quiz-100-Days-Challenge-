218d9af04d80ceeab39b18d32cce5547810b5cee
Oladipo Bolaji John <bolaji-oladipo@uiowa.edu>
2023-02-19 20:36:03 -0600

    Add files via upload

---
+++ Codes/PQ day 50.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 19 19:25:44 2023
+
+@author: boladipo
+"""
+
+li = ['in', 'as', 'if', 'be']
+
+
+print(li[1:][:2])
+
+print(li[1:2])
