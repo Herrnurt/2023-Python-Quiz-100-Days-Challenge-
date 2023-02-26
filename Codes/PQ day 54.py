bc8baf16783425a270f6b3f21ac188a759bb2911
Oladipo Bolaji John <bolaji-oladipo@uiowa.edu>
2023-02-25 23:33:07 -0600

    Add files via upload

---
+++ Codes/PQ day 54.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 23 23:34:02 2023
+
+@author: boladipo
+"""
+
---
+++ Codes/PQ day 55.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 25 23:19:20 2023
+
+@author: boladipo
+"""
+
+t1 = ('z', 'i', 'p')
+
+
+t2 = ()
+
+print(list(zip(t1,t2)))
